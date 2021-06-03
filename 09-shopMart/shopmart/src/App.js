0b18094399026841ac863f9ab278cc1c4e0fbddf
SA1MAANKHAN <nazma9832@gmail.com>
2021-06-04 00:37:29 +0530

    working on shomart

--- 09-shopMart/shopmart/src/App.js
+++ 09-shopMart/shopmart/src/App.js
@@ -2,12 +2,65 @@ import React from "react";
 import "./App.css";
 import Banner from "./components/Banner/banner.component";
 import Header from "./components/Header/header.component";
+import ProductCard from "./components/ProductCard/ProductCard.component";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Banner />
+      <div className="productCard__container">
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+        <ProductCard
+          name="Pizza"
+          price="4.99"
+          imageUrl="https://pngimg.com/uploads/pizza/pizza_PNG44084.png"
+        />
+      </div>
     </div>
   );
 }

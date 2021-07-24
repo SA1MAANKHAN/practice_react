ce15722316b361647f3a4c9fbb7411b09eb58720
SA1MAANKHAN <nazma9832@gmail.com>
2021-07-24 10:31:56 +0530

    started backend for whatsapp

--- 11-whatsapp/backend/server.js
+++ 11-whatsapp/backend/server.js
@@ -1,5 +1,6 @@
 // importing
 import express from "express";
+import mongoose from "mongoose";
 
 // app config
 const app = express();
@@ -8,10 +9,16 @@ const port = process.env.PORT || 9000;
 
 //db stuff
 
+const connectionUrl = `mongodb+srv://salmaan:5aiR6eJmJi19QpVE@cluster0.ipicg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionUrl, {});
+
 // api routes
-app.get("/", (req, res) => res.status(200).send("hello World"));
+app.get("/", (req, res) => res.status(200).send("helloooooooooo World"));
 
 // listener
 app.listen(port, () => {
   console.log("helloooooooonode from port ", port);
 });
+
+// some pass 5aiR6eJmJi19QpVE
